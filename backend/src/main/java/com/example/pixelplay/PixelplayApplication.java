23ba191b3c1eed398d1326f708dc82e5f8dd491c
GFA03 <florinalexandru302003@gmail.com>
2024-04-02 19:57:30 +0300

    created spring boot backend

---
+++ backend/src/main/java/com/example/pixelplay/PixelplayApplication.java
@@ -0,0 +1,13 @@
+package com.example.pixelplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixelplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixelplayApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/pixelplay/PixelplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pixelplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixelplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
