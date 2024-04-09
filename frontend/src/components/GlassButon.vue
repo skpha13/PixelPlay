ca08d54e61cb6ea6e36e5266a71b16974ed17c72
Adrian Mincu <61752094+skpha13@users.noreply.github.com>
2024-04-09 22:22:09 +0300

    feat: implemented button

---
+++ frontend/src/components/GlassButon.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const props = defineProps<{
+  title: string
+  link: string
+}>()
+</script>
+
+<template>
+  <RouterLink class="bg-white/10 backdrop-blur-sm hover:backdrop-blur-md hover:bg-white/20
+                 rounded-lg px-4 min-w-14
+                 border border-neutral-100 drop-shadow-sm
+                 font-bold"
+              :to="link">{{ props.title }}
+  </RouterLink>
+</template>
--- frontend/src/views/LandingPageView.vue
+++ frontend/src/views/LandingPageView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import Logo from "@/components/Logo.vue";
 import GamePreview from "@/components/GamePreview.vue";
+import GlassButon from "@/components/GlassButon.vue";
 
 </script>
 
