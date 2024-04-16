c99513a794dfef16ef65549892906e9f414d213d
Eduard Dumitrescul <dumitresculeduard@gmail.com>
2024-04-16 17:41:43 +0300

    feat: move testing for rook and queen

--- backend/src/test/java/com/example/pixelplay/chess/mechanics/QueenMechanicsTest.kt
+++ backend/src/test/java/com/example/pixelplay/chess/mechanics/QueenMechanicsTest.kt
@@ -6,13 +6,14 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 internal class QueenMechanicsTest {
+    private val mechanics = QueenMechanics()
+
     @BeforeEach
     fun setUp() {
     }
 
     @Test
     fun attackingIndexes() {
-        val mechanics: PieceMechanics = QueenMechanics()
         val squares: MutableList<Square> = mechanics.attacks(PositionGenerator.initialPosition(), Square(3, 1)).toMutableList()
         squares.sortWith(compareBy { it.getIndex() })
 
@@ -21,4 +22,39 @@ internal class QueenMechanicsTest {
             ArrayList(listOf(9, 11, 16, 17, 18, 24, 26, 27, 28, 29, 30, 31, 32, 33, 34, 41, 43, 49, 52))
         Assertions.assertEquals(expected, indexes)
     }
+
+    @Test
+    fun movesWithCapture() {
+        val moves: MutableList<Square> = mechanics.moves(PositionGenerator.customPosition(capture), Square(3, 4)).toMutableList()
+        val expected: MutableList<Square> = java.util.ArrayList(
+            java.util.List.of(
+                Square(2, 3),
+                Square(2, 4),
+                Square(2, 5),
+                Square(3, 0),
+                Square(3, 1),
+                Square(3, 2),
+                Square(3, 3),
+                Square(3, 5),
+                Square(3, 6),
+                Square(3, 7),
+            )
+        )
+
+        moves.sort()
+        expected.sort()
+
+        Assertions.assertEquals(expected, moves)
+    }
+
+    private val capture = arrayOf(
+        "........",
+        "........",
+        "...HKP..",
+        "....q...",
+        "...ppp..",
+        "........",
+        "........",
+        "........"
+    )
 }
--- backend/src/test/java/com/example/pixelplay/chess/mechanics/RookMechanicsTest.java
+++ backend/src/test/java/com/example/pixelplay/chess/mechanics/RookMechanicsTest.java
@@ -10,6 +10,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RookMechanicsTest {
+    private final PieceMechanics mechanics = new RookMechanics();
 
     @BeforeEach
     void setUp() {
@@ -17,7 +18,6 @@ class RookMechanicsTest {
 
     @Test
     void attackingIndexes() {
-        PieceMechanics mechanics = new RookMechanics();
         List<Square> squares = new ArrayList<>(mechanics.attacks(PositionGenerator.initialPosition(),   new Square(3, 1)));
         squares.sort(null);
         List<Integer> indexes = squares.stream().map(Square::getIndex).toList();
@@ -25,4 +25,36 @@ class RookMechanicsTest {
         List<Integer> expected = new ArrayList<>(List.of(9, 17, 24, 26, 27, 28, 29, 30, 31, 33, 41, 49));
         assertEquals(expected, indexes);
     }
+
+    @Test
+    void movesWithCapture() {
+        List<Square> moves = mechanics.moves(PositionGenerator.customPosition(capture), new Square(3, 4));
+        List<Square> expected = new ArrayList<>(List.of(
+                new Square(2, 4),
+                new Square(3, 2),
+                new Square(3, 3),
+                new Square(3, 5),
+                new Square(3, 6),
+                new Square(4, 4),
+                new Square(5, 4),
+                new Square(6, 4),
+                new Square(7, 4)
+        ));
+
+        moves.sort(null);
+        expected.sort(null);
+
+        assertEquals(expected, moves);
+    }
+
+    private final String[] capture = new String[] {
+            ".......K",
+            "........",
+            "...RP...",
+            "..K.r.Q.",
+            "...p.p..",
+            "........",
+            "........",
+            "........"
+    };
 }
