0253484edf185e47bafdf5eae541bc30e506dc34
Eduard Dumitrescul <dumitresculeduard@gmail.com>
2024-05-02 10:16:02 +0300

    ref: simple move validator

---
+++ backend/src/main/java/com/example/pixelplay/chess/moving/validator/PieceGetter.java
@@ -0,0 +1,22 @@
+package com.example.pixelplay.chess.moving.validator;
+
+import com.example.pixelplay.chess.base.Move;
+import com.example.pixelplay.chess.base.Piece;
+import com.example.pixelplay.chess.base.Position;
+import com.example.pixelplay.chess.exceptions.EmptySquareException;
+
+class PieceGetter {
+    private final Position position;
+
+    public PieceGetter(Position position) {
+        this.position = position;
+    }
+
+    public Piece tryToGetPiece(Move move) {
+        Piece piece = position.getPiece(move.start);
+        if (piece == null) {
+            throw new EmptySquareException("No piece to move");
+        }
+        return piece;
+    }
+}
