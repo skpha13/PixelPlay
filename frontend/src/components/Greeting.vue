fb58d5f685032da5c8d9628cea53c01d8a2f6559
Adrian Mincu <61752094+skpha13@users.noreply.github.com>
2024-05-11 18:49:45 +0300

    feat: connected backend to frontend and configured CORS

---
+++ frontend/src/components/Greeting.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import axios from 'axios'
+import {ref} from "vue";
+import Logo from "@/components/Logo.vue";
+
+const hello = ref<string>('');
+
+const fetchGreeting = async () => {
+  try {
+    const response = await axios.get('http://localhost:8080/greeting');
+    hello.value = response.data.content;
+  } catch (e) {
+    hello.value = ":(";
+  }
+}
+
+fetchGreeting();
+</script>
+
+<template>
+  <Logo :title="hello" />
+</template>
