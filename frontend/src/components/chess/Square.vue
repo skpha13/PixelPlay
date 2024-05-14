338b3a7cf3c826c16bfb846ac95e36a526633c37
Eduard Dumitrescul <dumitresculeduard@gmail.com>
2024-05-14 07:44:31 +0300

    feat: implemented basic board square

---
+++ frontend/src/components/chess/Square.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+  import {computed} from "vue";
+  import colors from "tailwindcss/colors";
+
+  const props = defineProps({
+    rank: {type:Number, required:true},
+    file: {type:Number, required:true},
+  })
+
+  const backgroundColor = computed(
+      () => {
+        if((props.file + props.rank) % 2 == 0) {
+          return colors.green["300"]
+        }
+        else {
+          return colors.sky["300"]
+        }
+      }
+  )
+
+
+</script>
+
+<template>
+  <div class="square aspect-square" :style="{backgroundColor}">
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
