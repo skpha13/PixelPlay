a71f811138a1cd0307ba3006a556e6728ec75c8a
Eduard Dumitrescul <dumitresculeduard@gmail.com>
2024-05-14 18:16:44 +0300

    ref: removed redundant directory

--- frontend/src/chess/piece/PieceModel.ts
+++ frontend/src/chess/piece/PieceModel.ts
@@ -11,18 +11,18 @@ export class PieceModel {
 
     public getImagePath(): string {
         switch(this.type) {
-            case "K":  return "src/chess/assets/chess/piece/white-king.png"
-            case "Q":  return "src/chess/assets/chess/piece/white-queen.png"
-            case "R":  return "src/chess/assets/chess/piece/white-rook.png"
-            case "B":  return "src/chess/assets/chess/piece/white-bishop.png"
-            case "H":  return "src/chess/assets/chess/piece/white-knight.png"
-            case "P":  return "src/chess/assets/chess/piece/white-pawn.png"
-            case "k":  return "src/chess/assets/chess/piece/black   -king.png"
-            case "q":  return "src/chess/assets/chess/piece/black-queen.png"
-            case "r":  return "src/chess/assets/chess/piece/black-rook.png"
-            case "b":  return "src/chess/assets/chess/piece/black-bishop.png"
-            case "h":  return "src/chess/assets/chess/piece/black-knight.png"
-            case "p":  return "src/chess/assets/chess/piece/black-pawn.png"
+            case "K":  return "src/chess/assets/piece/white-king.png"
+            case "Q":  return "src/chess/assets/piece/white-queen.png"
+            case "R":  return "src/chess/assets/piece/white-rook.png"
+            case "B":  return "src/chess/assets/piece/white-bishop.png"
+            case "H":  return "src/chess/assets/piece/white-knight.png"
+            case "P":  return "src/chess/assets/piece/white-pawn.png"
+            case "k":  return "src/chess/assets/piece/black   -king.png"
+            case "q":  return "src/chess/assets/piece/black-queen.png"
+            case "r":  return "src/chess/assets/piece/black-rook.png"
+            case "b":  return "src/chess/assets/piece/black-bishop.png"
+            case "h":  return "src/chess/assets/piece/black-knight.png"
+            case "p":  return "src/chess/assets/piece/black-pawn.png"
             default: return ""
         }
     }
